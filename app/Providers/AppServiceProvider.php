d7ce2cbd40702947158603fddd86df656fa0a6b5
Yusuf Burhani <yusufburhani20@gmail.com>
2026-09-11 09:29:27 +0700

    update

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -19,6 +19,10 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
+        if (config('app.env') === 'production' || request()->header('x-forwarded-proto') == 'https') {
+            \Illuminate\Support\Facades\URL::forceScheme('https');
+        }
+
         \Illuminate\Support\Facades\View::composer('*', function ($view) {
             try {
                 $settings = \App\Models\Setting::allAsArray();
